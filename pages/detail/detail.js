e127e276fcd5c16c3327daa2c7798ad275feef6d
Enze <56312150@qq.com>
2020-04-29 10:07:44 +0800

    按钮跳转

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -12,55 +12,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onUnload:function(){
+    // 获取首页的页面对象
+    const pages = getCurrentPages()
+    const home = pages[pages.length-2]
+    home.setData({
+      name:'Hi Ciogg'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBackHome(){
+    wx.navigateBack({
+      delta:1
+    })
   }
+
 })
